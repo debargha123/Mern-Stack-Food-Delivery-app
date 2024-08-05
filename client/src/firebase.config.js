3910240213e67420139ba341486fa235a5002965
Debargha Saha <debsaha158@gmail.com>
2024-08-05 23:03:55 +0530

    first commit

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -4,12 +4,12 @@ import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "restaurant-d1b16.firebaseapp.com",
+    authDomain: "food-delivery-app-61cc9.firebaseapp.com",
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: "restaurant-d1b16",
-    storageBucket: "restaurant-d1b16.appspot.com",
-    messagingSenderId: "819814547317",
-    appId: "1:819814547317:web:537afe00c6ffc6d4f65d3c"
+    projectId: "food-delivery-app-61cc9",
+    storageBucket: "food-delivery-app-61cc9.appspot.com",
+    messagingSenderId: "348954940204",
+    appId: "348954940204",
 };
 
 const app = getApps.length > 0 ? getApp() :  initializeApp(firebaseConfig);
